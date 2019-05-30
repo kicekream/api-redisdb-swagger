eb9d53fe5f08b27d7a7da808e24c1c2b7d6dd4d4
kicekream <kicekream@gmail.com>
2019-05-30 22:06:43 +0100

    updated environment variables for redisdb

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 //Redis DataBase
 const express = require("express");
 const redis = require("redis");
-const path = require('path');
-const dotenv = require('dotenv');
+const path = require("path");
+const dotenv = require("dotenv");
 const swaggerUi = require("swagger-ui-express");
-const YAML = require('yamljs')
-const swaggerDocument = YAML.load(path.join(process.cwd(), './swagger.yaml'))
+const YAML = require("yamljs");
+const nconf = require("nconf");
+const swaggerDocument = YAML.load(path.join(process.cwd(), "./swagger.yaml"));
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 8080
+nconf
+  .argv()
+  .env()
+  .file("keys.json");
+const PORT = process.env.PORT || 8080;
 
 //Initialise Redis Database
-const redisClient = redis.createClient();
+const redisClient = redis.createClient(
+  nconf.get("redisPort") || "6379",
+  nconf.get("redisHost") || "127.0.0.1",
+  {
+    auth_pass: nconf.get("redisKey"),
+    return_buffers: true
+  }
+);
 redisClient.on("connect", () => console.log("Redis dataBase started"));
 
-const user = require('./routes/user')(redisClient);
+const user = require("./routes/user")(redisClient);
 
 app.use(express.json());
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/user', user)
+app.use("/user", user);
 
-app.get('/', (req, res) => {
-    res.redirect('/docs');
-})
+app.get("/", (req, res) => {
+  res.redirect("/docs");
+});
 
 app.listen(PORT, () => console.log(`App started on port ${PORT}`));
